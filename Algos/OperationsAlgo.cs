7ab3739ea9eb8d2471a6326d404f1d1e3b0f64d4
ZakariaOB <boukhriszakaria@gmail.com>
2021-04-21 02:49:45 +0000

    First commit : Project creation

---
+++ Algos/OperationsAlgo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace TrainningConsolePoho.Algos
+{
+    public class OperationsAlgo
+    {
+        public void GetResponsability()
+        {
+            Console.WriteLine("Sum algo class");
+        }
+
+        public string GetResponsabilityString()
+        {
+            return "Sum algo class from string function";
+        }
+
+        public int Sum(int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return 0;
+
+            int result = 0;
+            foreach (int value in values)
+            {
+                result += value;
+            }
+            return result;
+        }
+
+
+        public int Max(int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return 0;
+
+            int maxValue = 0;
+            foreach (int value in values)
+            {
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+            return maxValue;
+        }
+
+        public int MaxOptimized(int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return 0;
+
+            return values.Max();
+        }
+
+        public int SumOptimized(int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return 0;
+
+            return values.Sum();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TrainningConsolePoho.Algos;
+
+namespace TrainningConsolePoho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationsAlgo opAlgo = new OperationsAlgo();
+
+            // OutPut input
+            opAlgo.GetResponsability();
+            string outPut = opAlgo.GetResponsabilityString();
+            Console.WriteLine(outPut);
+
+            // Sum value version 1
+            int[] values = new[] { 1, 2, 3, 10, 12 };
+
+            int resultSum = opAlgo.Sum(values);
+            Console.WriteLine(resultSum);
+
+            // Max  
+            int maxValue = opAlgo.Max(values);
+            int maxOptimizedValue = opAlgo.Max(values);
+
+            Console.WriteLine("Max value : " + maxValue);
+            Console.WriteLine("Max optimized value : " + maxOptimizedValue);
+
+            Console.ReadKey();
+        }
+    }
+}
